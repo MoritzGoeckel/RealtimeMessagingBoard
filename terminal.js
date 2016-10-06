ef818cbb89536a328b456178ef187b26bd231cd7
Moritz Göckel <Moritz Gckel>
2016-10-06 21:50:51 +0900

    finished the terminal

--- terminal.js
+++ terminal.js
@@ -1,3 +1,5 @@
+ ctrlDown = false;
+
 $().ready(function(){
 	
 	$( "body" ).keydown(function(event) {
@@ -5,28 +7,63 @@ $().ready(function(){
 		processKey(event.key);
 	});
 	
-	console.log("Init finished");
+	$("body").keydown(function(e) {
+        if (e.keyCode == 17 || e.keyCode == 91) ctrlDown = true;
+    }).keyup(function(e) {
+        if (e.keyCode == 17 || e.keyCode == 91) ctrlDown = false;
+    });
 	
+	$("body").on('paste', function(e) {
+		var pasteData = e.originalEvent.clipboardData.getData('text');
+		for(var i = 0; i < pasteData.length; i++)
+			processKey(pasteData[i], true);
+	});
+	
+	setInterval(function()
+		{ 
+			showBlock = (showBlock ? false : true);
+			if(moving)
+			{
+				showBlock = true;
+				moving = false;
+			}
+			
+			renderInput(); 
+		}, 500);
+	
+	console.log("Init finished");
 });
 
+inputIndex = 0;
 inputhistoryIndex = 0;
 inputHistory = [];
 input = "";
-function processKey(key)
+showBlock = true;
+moving = false;
+
+function processKey(key, ingoreCtrl = false)
 {
 	if(key == "Backspace")
 	{
-		input = input.substr(0, input.length - 1);
+		input = input.substr(0, input.length - inputIndex - 1) + input.substr(input.length - inputIndex);
 		inputhistoryIndex = 0;
+		
+		if(inputIndex > input.length)
+			inputIndex = input.length;
 	}
 	else if(key == "Enter")
 	{
+		if((inputHistory[0] == "" || inputHistory[0] == " "))
+			inputHistory.shift();
+		
 		if(inputHistory[0] != input)
 			inputHistory.unshift(input);
 		
 		inputhistoryIndex = 0;
 		submitInput(input);
 		input = "";
+		
+		inputIndex = 0;
 	}
 	else if(key == "ArrowUp")
 	{
@@ -38,6 +75,8 @@ function processKey(key)
 			inputhistoryIndex++;
 			input = inputHistory[inputhistoryIndex];
 		}
+		
+		inputIndex = 0;
 	}
 	else if(key == "ArrowDown")
 	{
@@ -49,25 +88,68 @@ function processKey(key)
 		
 		if(inputhistoryIndex == 0 && (inputHistory[0] == "" || inputHistory[0] == " "))
 			inputHistory.shift();
+		
+		inputIndex = 0;
 	}
-	else
+	else if(key == "ArrowLeft")
+	{
+		if(inputIndex < input.length)
+			inputIndex++;
+	}
+	else if(key == "ArrowRight")
+	{
+		if(inputIndex > 0)
+			inputIndex--;
+	}
+	else if(key.length == 1 && (ctrlDown == false || ingoreCtrl))
 	{
 		historyIndex = 0;
 		input += key;
 	}
 	
-	console.log(inputhistoryIndex);
-	console.log(inputHistory);
+	moving = true;
 	
 	renderInput();
 }
 
 function renderInput()
 {
-	$("#inputLine").html(input + "&block;");
+	$("#inputField").html("<span class='c_message'>" + input.substr(0, input.length - inputIndex) + (showBlock ? "<span class='c_interactive'>&block;</span>" : "  ") + input.substr(input.length - inputIndex) + "</span>");
 }
 
 function submitInput(line)
 {
-	//
+	if(line != "" && line != " ")
+	{
+		printLine("-> " + line, "msg");
+		for(var i = 0; i < listeners.length; i++)
+			listeners[i](line);
+	}
+}
+
+listeners = [];
+function addInputListener(func)
+{
+	listeners.push(func);
+}
+
+function printLine(str, type)
+{
+	var start = "<span class='c_message'>";
+	
+	if(type == "int")
+		start = "<span class='c_interactive'>";
+	
+	if(type == "imp")
+		start = "<span class='c_important'>";
+	
+	if(type == "hig")
+		start = "<span class='c_highlight'>";
+	
+	if(type == "msg")
+		start = "<span class='c_message'>";
+	
+	var end = "</span>";
+	
+	$("#terminalField").html($("#terminalField").html() + start + str + end + "<br />");
 }
