5837a7a73fa558adfbe6b499a7a9194618c14560
Moritz Göckel <Moritz Gckel>
2016-10-06 23:56:03 +0900

    Created a simple forum

---
+++ forum.js
@@ -0,0 +1,54 @@
+setMaxLines(10);
+
+printLine("### Welcome ###", "hig");
+
+choosRoomMenu();
+
+addInputListener(function(line){ 
+
+	nextState(line);
+
+});
+
+function choosRoomMenu()
+{
+	printLine("Choose room", "imp");
+	nextState = openChat;
+	
+	$.getJSON( "http://localhost:3000/rooms", function( data ) {
+		$.each( data, function( key, val ) {
+			printLine(val.id + ": " + val.name + " | " + val.online + "/" + val.members, "hig");
+		});
+	});
+}
+
+function openChat(line)
+{
+	printLine("Entering " + line, "imp");
+	nextState = inChat;
+	
+	currentRoom = line;
+	
+	$.getJSON( "http://localhost:3000/msgs/" + line, function( data ) {
+		$.each( data, function( key, val ) {
+			printLine(val.username + ": " + val.content, "hig");
+		});
+	});
+}
+
+currentRoom = "none";
+function inChat(line)
+{
+	if(line == "exit")
+		choosRoomMenu();
+	else 
+	{
+		//Write msg
+		var msg = {
+			"userid": -1,
+			"content": line
+		};
+		
+		$.post( "http://localhost:3000/insertmsg/" + currentRoom, encodeURIComponent(JSON.stringify(msg)));
+	}
+}
---
+++ node/server.js
@@ -0,0 +1,92 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+nextUserId = 0;
+nextRoomId = 0;
+nextMessageId = 0;
+
+rooms = [
+	{
+		"name": "Info",
+		"id": nextRoomId++,
+		"members": 0,
+		"online": 0
+	},
+	{
+		"name": "Spam",
+		"id": nextRoomId++,
+		"members": 0,
+		"online": 0
+	}
+];
+
+//First dimension is the roomid
+msgs = [
+	[
+		{
+			"username": "System",
+			"userid": nextUserId++,
+			"content": "Keep on believing, have fun during the day and make all the things",
+			"msgid": nextMessageId++,
+			"date": "2015-07-25T01:44:31 -09:00"
+		}
+	],
+	[
+	
+	]
+];
+
+const server = http.createServer((req, res) => {
+	
+	var path = req.url;
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/json');
+	res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	
+	console.log("Request for -> " + path);
+	if(path == undefined)
+		res.end("Undefined request... :)\n");
+	else if(path == "/rooms")
+		res.end(JSON.stringify(rooms));
+	else if(path.startsWith("/msgs/"))
+	{
+		var params = path.split("/");
+		var id = params[params.length - 1];
+		console.log("Delivering id: " + id);
+		res.end(JSON.stringify(msgs[id]));
+	}
+	else if(path.startsWith("/insertmsg/") && req.method == 'POST')
+	{
+		var params = path.split("/");
+		var id = params[params.length - 1];
+		
+		req.on('data', function (data) {
+			data = decodeURIComponent(data);
+			
+			var jsonObject = JSON.parse(data);
+			
+			jsonObject.username = "Anonymous";
+			jsonObject.date = new Date().toUTCString();
+			jsonObject.msgid = nextMessageId++;
+			
+			console.log("Insert Msg:");
+			console.log(jsonObject); 
+			
+			msgs[id].unshift(jsonObject);
+			
+			req.connection.destroy();
+		});
+		
+		res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('Recieved msg');
+	}
+	else
+		res.end("Not found... :)\n");
+});
+
+server.listen(port, hostname, () => {
+  console.log('Server running at http://${hostname}:${port}/');
+});
